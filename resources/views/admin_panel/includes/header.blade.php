246f8c1aa832eda72455eab132d2ee392824d935
Kashan Shaikh <shaikhkashan670@gmail.com>
2025-07-21 14:42:19 +0500

    ss

--- resources/views/admin_panel/includes/header.blade.php
+++ resources/views/admin_panel/includes/header.blade.php
@@ -1,29 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="utf-8" />
     <title>Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="Coderthemes" name="author    " />
+    <meta content="Coderthemes" name="author" />
 
-    <link rel="shortcut icon" href="{{ env('APP_URL') }}public/assets/images/Ict_logo.png">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/Ict_logo.png') }}">
 
-    <link href="{{ env('APP_URL') }}public/assets/vendor/daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css">
-    <link href="{{ env('APP_URL') }}public/assets/vendor/jsvectormap/jsvectormap.min.css" rel="stylesheet" type="text/css">
-    <script src="{{ env('APP_URL') }}public/assets/js/hyper-config.js"></script>
+    <!-- Vendor CSS -->
+    <link href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/vendor/jsvectormap/jsvectormap.min.css') }}" rel="stylesheet" type="text/css">
+    
+    <!-- Main CSS -->
+    <link href="{{ asset('assets/css/vendor.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app-saas.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 
-    <link href="{{ env('APP_URL') }}public/assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ env('APP_URL') }}public/assets/css/app-saas.min.css" rel="stylesheet" type="text/css" id="app-style" />
-    <link href="{{ env('APP_URL') }}public/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-
+    <!-- DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
-    <!-- <link rel="stylesheet" href="{{ env('APP_URL') }}public/assets/css/datatables.bootstrap5.css" /> -->
-    <link rel="stylesheet" href="{{ env('APP_URL') }}public/assets/css/responsive.bootstrap5.css" />
-    <link rel="stylesheet" href="{{ env('APP_URL') }}public/assets/css/datatables.checkboxes.css" />
+    <!-- <link rel="stylesheet" href="{{ asset('assets/css/datatables.bootstrap5.css') }}" /> -->
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.bootstrap5.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/datatables.checkboxes.css') }}" />
+
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <!-- JS Config -->
+    <script src="{{ asset('assets/js/hyper-config.js') }}"></script>
 </head>
+
 <style>
     .dataTables_wrapper .dataTables_filter input {
         margin-bottom:10px 
